a83c2348c490b4d8ceb73b03613a8d83d646a5c5
antoine-utopios <antoine@utopios.net>
2025-04-14 13:59:37 +0200

    Ajoutez des fichiers projet.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public virtual DbSet<Cat> Cats { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ Entities/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Entities
+{
+    public class Cat
+    {
+        // [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        public string Breed { get; set; }
+    }
+}
